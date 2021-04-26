3b8bc379fbbd06fdefa30ccc26a5d144eaa350fa
hollaze <deperiers.a@gmail.com>
2021-04-26 22:22:53 +0200

    checking

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,20 +4,19 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *normal;
-	listint_t *fast = fast->next;
+	listint_t *fast = list;
 
-	if (normal == NULL || fast == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
 
-	while (list != NULL)
+	while (fast && list && fast->next)
 	{
-		normal = normal->next;
+		list = list->next;
 		fast = fast->next->next;
 
-		if (normal == fast)
+		if (list == fast)
 			return (1);
 	}
 
-return (0);
+	return (0);
 }
