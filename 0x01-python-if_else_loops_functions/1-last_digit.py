dabe8f15ba0a2c51fe30647f69e68bb6500fe41a
hollaze <deperiers.a@gmail.com>
2021-04-27 18:52:10 +0200

    Fixing elif condition

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,5 @@ if n > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, n))
 elif n == 0:
     print("Last digit of {} is {} and is 0".format(number, n))
-elif n < 6 and number != 0:
+elif n < 6 and n!= 0:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, n))
