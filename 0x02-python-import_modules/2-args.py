7190a3bae4eeba5f8bd6e7bc5688e86dd4eb701b
hollaze <deperiers.a@gmail.com>
2021-04-29 13:45:36 +0000

    removing execution when importation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-length = len(sys.argv) - 1
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv) - 1
 
-if length == 1:
-    print("{:d} argument:".format(length))
-else:
-    print("{:d}".format(length), "arguments:" if length != 0 else "arguments.")
+    if length == 1:
+        print("{:d} argument:".format(length))
+    else:
+        print("{:d}".format(length), "arguments:" if length != 0 else "arguments.")
 
-for i in range(0, length, 1):
-    print("{:d}:".format(i + 1), sys.argv[i + 1])
+    for i in range(0, length, 1):
+        print("{:d}:".format(i + 1), argv[i + 1])
