1d0cedc2685578b5e1e30b658030be1f42813663
hollaze <deperiers.a@gmail.com>
2021-04-29 15:01:34 +0000

    Fixing pep8

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,26 +4,25 @@ if __name__ == "__main__":
     from sys import argv, exit
 
     result = 0
-    sign = argv[2]
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    elif sign != "+" and sign != "-" and sign != "*" and sign != "/":
+    if argv[2] == "+":
+        result = add(int(argv[1]), int(argv[3]))
+
+    elif argv[2] == "-":
+        result = sub(int(argv[1]), int(argv[3]))
+
+    elif argv[2] == "*":
+        result = mul(int(argv[1]), int(argv[3]))
+
+    elif argv[2] == "/":
+        result = div(int(argv[1]), int(argv[3]))
+
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
-    if sign == "+":
-        result = add(int(argv[1]), int(argv[3]))
-
-    elif sign == "-":
-        result = sub(int(argv[1]), int(argv[3]))
-
-    elif sign == "*":
-        result = mul(int(argv[1]), int(argv[3]))
-
-    elif sign == "/":
-        result = div(int(argv[1]), int(argv[3]))
-
-    print(argv[1], sign, argv[3], "=", result)
+    print(argv[1], argv[2], argv[3], "=", result)
