f0616dbf16ad55294a8e1fb05bb9d7a822dd5689
hollaze <deperiers.a@gmail.com>
2021-04-29 13:41:23 +0000

    Fixing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 import sys
-length = len(sys.argv) -1
+length = len(sys.argv) - 1
 
 if length == 1:
     print("{:d} argument:".format(length))
-elif length > 1:
-    print("{:d} arguments:".format(length))
+else:
+    print("{:d}".format(length), "arguments:" if length != 0 else "arguments.")
 
 for i in range(0, length, 1):
     print("{:d}:".format(i + 1), sys.argv[i + 1])
-
-if length == 0:
-    print("0 arguments.")
