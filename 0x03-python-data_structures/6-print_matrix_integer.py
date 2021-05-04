37a04e9fb6ad8b5cc60cb3d19faf855c139ea77d
hollaze <deperiers.a@gmail.com>
2021-05-04 16:32:46 +0200

    Checking

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end='')
+
+            if col % 3 == 0:
+                print()
