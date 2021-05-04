8ccf56dc0f6d53f0b12efd76ffb5c8b313092c76
hollaze <deperiers.a@gmail.com>
2021-05-04 16:34:06 +0200

    Adding space between numbers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            print("{:d}".format(col), end='')
+            print("{:d} ".format(col), end='')
 
             if col % 3 == 0:
                 print()
