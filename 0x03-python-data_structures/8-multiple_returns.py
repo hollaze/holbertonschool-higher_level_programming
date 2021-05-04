d14987bda55c2ef985566036dd1fde49a7909f7e
hollaze <deperiers.a@gmail.com>
2021-05-04 21:49:50 +0200

    Fixing if condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     tuple_s = (len(sentence), sentence[0])
-    if not sentence:
+    if len(sentence) == 0:
         return (0, None)
 
     return tuple_s
