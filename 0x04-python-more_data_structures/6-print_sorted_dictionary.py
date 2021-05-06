6fef4b9b44666eb0202958eabfe11e6c88fb28bb
hollaze <deperiers.a@gmail.com>
2021-05-06 19:22:04 +0200

    Adding helpfull comments

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
         for i in list(sorted(a_dictionary)):
-            '#i is key'
+            '#i is key and a_dictionary[i] is like: [i] is element of i'
+            '#So [values] is element of key'
             print(i, a_dictionary[i], sep=": ")
