666dbd1cbf04b4f70026992c93eb660865d70964
hollaze <deperiers.a@gmail.com>
2021-05-06 18:22:59 +0200

    Modifying output

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary:
-        [print(i) for i in list(sorted(a_dictionary.items()))]
+        for i in list(sorted(a_dictionary)):
+            '#i is key'
+            print(i, a_dictionary[i], sep=": ")
