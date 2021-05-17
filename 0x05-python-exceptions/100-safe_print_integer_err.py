fa600fc844fa667f2945d9374476ecca8f6fb646
hollaze <deperiers.a@gmail.com>
2021-05-17 12:56:17 +0200

    except is now Exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as Ve:
-        print("Exception:", Ve, file=sys.stderr)
+    except Exception as exception:
+        print("Exception:", exception, file=sys.stderr)
         return False
     return True
