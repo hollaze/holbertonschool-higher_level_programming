4c01070fd441da08d574fd866204768411d3862b
hollaze <deperiers.a@gmail.com>
2021-05-17 12:54:09 +0200

    Adding Exception into print

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except ValueError as Ve:
-        print(Ve, file=sys.stderr)
+        print("Exception:", Ve, file=sys.stderr)
         return False
     return True
