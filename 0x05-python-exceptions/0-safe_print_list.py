3a6ab022f9d823bd92449b4ede304426c85cc4ff
hollaze <deperiers.a@gmail.com>
2021-05-17 09:08:31 +0200

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in my_list[:x]:
+        try:
+            print(i, end='')
+        except:
+            break
+    print()
+    return i
