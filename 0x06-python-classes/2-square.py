6323642d9183008285895d934913479415192916
hollaze <deperiers.a@gmail.com>
2021-05-18 18:26:12 +0200

    Checking

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square:
+    """ Class:
+            empty class that defines a square
+
+        Instance:
+                size: size of the square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
