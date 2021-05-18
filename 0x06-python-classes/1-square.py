86efc144e8f2438b5e08cd8da6b065a6a3f75fa6
hollaze <deperiers.a@gmail.com>
2021-05-18 18:13:00 +0200

    Checking

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square:
+    """ Class:
+            empty Square class that defines a square
+
+        Instance:
+            size: size of the square
+    """
+    def __init__(self, __size=None):
+        self.__size = 0
