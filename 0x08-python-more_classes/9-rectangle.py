877eec34a1006c2e62c822d101049cca79a5fc5f
hollaze <deperiers.a@gmail.com>
2021-05-26 02:35:17 +0200

    test

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -94,5 +94,6 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
-        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+        
