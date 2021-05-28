a2fc2ef2ba9b681fb493c99f89d4ba01745716da
hollaze <deperiers.a@gmail.com>
2021-05-28 15:38:51 +0200

    adding comments

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-
+''' text indentation function '''
 
 def text_indentation(text):
-
+    '''
+    indent every time there is a ',' , ':' , '?'
+    prints a new line
+    
+        Parameter:
+                text    (str)
+                
+        Returns:
+                void
+    '''
     i = 0
 
     if not isinstance(text, str):
