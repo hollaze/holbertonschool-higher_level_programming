5cfe6936102c28072163f5ef3fdce6d5ea8746c3
hollaze <deperiers.a@gmail.com>
2021-06-01 22:54:51 +0200

    Checking

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,6 @@ def inherits_from(obj, a_class):
     '''
     Only sub class of
     '''
-    if issubclass(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
