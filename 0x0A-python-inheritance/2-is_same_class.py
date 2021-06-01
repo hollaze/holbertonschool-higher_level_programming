7e4d0721b25d3673b92c78cd2ba5ba39206dccde
hollaze <deperiers.a@gmail.com>
2021-06-01 22:13:48 +0200

    Checking

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' same class def'''
+
+def is_same_class(obj, a_class):
+    '''
+    check if obj is same of of a_class
+    '''
+    if type(obj) is a_class:
+        return True
+    return False
