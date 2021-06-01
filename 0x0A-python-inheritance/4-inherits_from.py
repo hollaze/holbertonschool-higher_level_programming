1d1c1d1dd79eeb4be48f6b147eb1332ae60ec98a
hollaze <deperiers.a@gmail.com>
2021-06-01 22:31:26 +0200

    checking

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' inherits from'''
+
+
+def inherits_from(obj, a_class):
+    '''
+    Only sub class of
+    '''    
+    if issubclass(obj, a_class):
+        return True
+    return False
