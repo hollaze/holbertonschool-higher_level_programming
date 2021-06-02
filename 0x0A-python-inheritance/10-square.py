e0a1edf0e3372e44ea420411140d34426911ab50
hollaze <deperiers.a@gmail.com>
2021-06-02 14:06:37 +0200

    Adding class coms

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     '''
     defines a square with inheritance
     '''
-    
+
     def __init__(self, size):
         self.__size = size
         self.integer_validator("size", size)
