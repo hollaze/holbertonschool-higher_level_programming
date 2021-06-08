cd3bf1bb0b8f7ac4a9a336005067bdc2c331e19f
hollaze <deperiers.a@gmail.com>
2021-06-08 11:58:49 +0200

    adding square function

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Square class """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Defines a Square
+        Inherited from Rectangle
+
+    Args:
+        Rectangle (class): Defines a Rectangle
+
+    Methods:
+        __str__
+        validate_int
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        gives informations of a square
+
+        Returns:
+            [str]: informations
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def validate_int(self, name, number):
+        """
+        check if a number is an int
+
+        Parameters:
+            self    (Rectangle)
+            name    (str)
+            number  (int)
+        """
+        if type(number) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+    """ getter setter """
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.validate_int("width", value)
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
