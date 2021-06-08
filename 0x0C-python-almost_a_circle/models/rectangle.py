9f926047cfbab3454d1ed0e7a0e6a513f2229964
hollaze <deperiers.a@gmail.com>
2021-06-08 10:54:29 +0200

    l61 self.x -> self.x - 1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -58,7 +58,7 @@ class Rectangle(Base):
         """
         print("\n" * self.__y, end='')
         for row in range(self.__height):
-            print(" " * self.__x, "#" * self.__width)
+            print(" " * (self.__x - 1), "#" * self.__width)
 
     def update(self, *args, **kwargs):
         """
