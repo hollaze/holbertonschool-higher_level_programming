64048bbc9df1c2866db18b26dbf3aae0e95ba6f5
hollaze <deperiers.a@gmail.com>
2021-06-08 15:49:06 +0200

    check

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,27 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    def update(self, *args, **kwargs):
+        """
+        update arguments
+
+        Parameters:
+            self    (Rectangle)
+            args    (Any)
+            kwargs  (dict)
+        """
+        if args and args is not None:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                pass
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
     def __str__(self):
         """
         gives informations of a square
