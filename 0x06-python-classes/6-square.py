282557a02728307aecd3d2172eef8c5c04f58c70
hollaze <deperiers.a@gmail.com>
2021-07-02 00:07:57 +0200

    l:25 new return, fixing multiple spaces

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -22,6 +22,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
+            return
         for i in range(self.__position[1]):
             print()
         for row in range(self.__size):
