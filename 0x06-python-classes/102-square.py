51e228fb6a7c415396f16f7b5f077ebc10f14565
hollaze <deperiers.a@gmail.com>
2021-07-04 10:00:53 +0200

    comparing instances with each other

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square:
+    """ Class:
+            empty class that defines a square
+
+        Instance:
+                size: size of the square
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def __lt__(self, other):
+        return self.__size < other.__size
+
+    def __le__(self, other):
+        return self.__size <= other.__size
+
+    def __eq__(self, other):
+        return self.__size == other.__size
+
+    def __ne__(self, other):
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        return self.__size >= other.__size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value=0):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
