c040eb4e5997538f4de08b607fc25f930423bd7a
hollaze <deperiers.a@gmail.com>
2021-07-13 18:51:02 +0200

    list of records with same score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- list the number of records with the same score in second_table
 
-SELECT score, COUNT(*) 'number' FROM second_table GROUP BY score;
+SELECT score, COUNT(*) 'number' FROM second_table GROUP BY score ORDER BY number DESC;
