7e6613a7e35cd21b919b59e182c84f3a8b9e124f
hollaze <deperiers.a@gmail.com>
2021-07-18 04:11:24 +0200

    reverse commit

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(BaseGeometry):
     Subclass of BaseGeometry
     '''
 
-    def __init__(self, width: int, height):
+    def __init__(self, width, height):
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
