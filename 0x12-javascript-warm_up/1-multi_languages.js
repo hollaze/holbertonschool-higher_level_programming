ab07a205b18a155235c64e80e6ce888d9bfc4bff
hollaze <deperiers.a@gmail.com>
2021-08-02 15:10:53 +0200

    prints 3 strings

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -4,4 +4,6 @@
  * print 3 strings
  */
 
-console.log('C is fun', '\nPython is cool', '\nJavascript is amazing');
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
