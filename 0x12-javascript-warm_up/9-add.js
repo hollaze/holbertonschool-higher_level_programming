d3b4930108cec8bf1f8c5a58b823d2ffb7965b66
hollaze <deperiers.a@gmail.com>
2021-08-02 17:33:47 +0200

    fix semistandart

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const argv = process.argv;
-let res = 0;
+const res = 0;
 
-function add(a, b) {
-    if (a === undefined || isNaN(parseInt(a)) ||
+function add (a, b) {
+  if (a === undefined || isNaN(parseInt(a)) ||
         b === undefined || isNaN(parseInt(b))) {
-        console.log('NaN');
-    }
-    else {
-        console.log(a + b);
-    }
+    console.log('NaN');
+  } else {
+    console.log(a + b);
+  }
 }
 
 add(argv[2], argv[3]);
