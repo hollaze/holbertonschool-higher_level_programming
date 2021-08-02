2b703414fb817900f4742e6bb1be6ccea22d1a56
hollaze <deperiers.a@gmail.com>
2021-08-02 17:54:00 +0200

    fix semistandart

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-function factorial(n) {
-    let res = 1;
-    if (n > 0) {
-        res = factorial(n - 1) * n;
-    }
-    return res;
+function factorial (n) {
+  let res = 1;
+  if (n > 0) {
+    res = factorial(n - 1) * n;
+  }
+  return res;
 }
 
 console.log(factorial(parseInt(argv[2])));
