2b7deb21420f0e29ff1c48432d7d4a6af8bb5bfd
hollaze <deperiers.a@gmail.com>
2021-08-02 16:38:43 +0200

    fix semistandart

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let argv = process.argv;
+const argv = process.argv;
 
 if (isNaN(parseInt(argv[2]))) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + parseInt(argv[2]));
+  console.log('My number: ' + parseInt(argv[2]));
 }
