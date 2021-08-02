07965af1ef2633e9b1d2cd921aabc9f2d261182a
hollaze <deperiers.a@gmail.com>
2021-08-02 17:37:36 +0200

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const argv = process.argv;
-const res = 0;
 
 function add (a, b) {
+  const res = parseInt(a) + parseInt(b);
   if (a === undefined || isNaN(parseInt(a)) ||
         b === undefined || isNaN(parseInt(b))) {
     console.log('NaN');
   } else {
-    console.log(a + b);
+    console.log(res);
   }
 }
 
