ea56dcb7d9d880a925d9ba20839f66826fb84ca5
hollaze <deperiers.a@gmail.com>
2021-08-02 17:03:04 +0200

    I love C more than javascript

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,9 @@
 const argv = process.argv;
 
 if (argv[2] === undefined || isNaN(parseInt(argv[2]))) {
-    console.log('Missing number of occurrences');
-}
-else {
-    for (let i = 0; i < parseInt(argv[2]); i++) {
-        console.log('C is fun');
-    }
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(argv[2]); i++) {
+    console.log('C is fun');
+  }
 }
