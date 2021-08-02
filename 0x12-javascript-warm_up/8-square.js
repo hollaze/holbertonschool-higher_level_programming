245712ae1f2301900bae3620de70152dd6853329
hollaze <deperiers.a@gmail.com>
2021-08-02 17:22:30 +0200

    print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const argv = process.argv;
+const stringX = 'X'; let str = '';
+
+if (argv[2] === undefined || isNaN(parseInt(argv[2]))) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(argv[2]); i++) {
+    str += stringX;
+    for (let j = 0; j < parseInt(argv[2]); j++) {
+      if (i == parseInt(argv[2]) - 1) {
+        console.log(str);
+      }
+    }
+  }
+}
