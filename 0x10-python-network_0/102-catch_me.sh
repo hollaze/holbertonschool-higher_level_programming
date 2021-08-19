95faf9ea6ee5fd1550f80482a6c3d93a7069ae4b
hollaze <deperiers.a@gmail.com>
2021-08-19 16:46:47 +0200

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #request to port 5000, output: You got me!
-curl -s 0.0.0.0:5000/catch_me -o /dev/null -w "You got me!"
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
