f35ea71f44f7dfbc55cf8e71923ab8d31f8c7848
hollaze <deperiers.a@gmail.com>
2021-08-19 19:47:33 +0200

    find peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Find a peak"""
+
+
+def find_peak(list_of_integers):
+    """Function to find a peak"""
+
+    if len(list_of_integers) <= 0:
+        return None
+    else:
+        list_of_integers.sort()
+        return list_of_integers[-1]
