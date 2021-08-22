e950a90f59e4dbeea458645f6b1e114ed893c071
hollaze <deperiers.a@gmail.com>
2021-08-22 15:31:29 +0200

    display header response of X-Request-Id

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,4 +5,4 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    print(requests.get(argv[1]).headers["X-Request-Id"])
+    print(requests.get(argv[1]).headers.get("X-Request-Id"))
