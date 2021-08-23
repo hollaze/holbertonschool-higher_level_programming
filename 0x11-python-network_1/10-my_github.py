f686287bffd5071de7d49a15f5983c550cadeb3d
hollaze <deperiers.a@gmail.com>
2021-08-23 19:23:19 +0200

    check

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""using github api"""
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    
+    response = get(url, auth=(argv[1], argv[2]))
+    json = response.json()
+    print(json.get('id'))
+    
