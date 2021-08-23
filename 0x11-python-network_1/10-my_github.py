667197b690001dd8a1e602c10e3f615f7e334a55
hollaze <deperiers.a@gmail.com>
2021-08-23 19:23:43 +0200

    check

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,8 +6,7 @@ from sys import argv
 
 if __name__ == "__main__":
     url = "https://api.github.com/user"
-    
+
     response = get(url, auth=(argv[1], argv[2]))
     json = response.json()
     print(json.get('id'))
-    
