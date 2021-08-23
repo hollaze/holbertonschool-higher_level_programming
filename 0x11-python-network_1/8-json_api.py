efebe6d98aa4b825b84ebdef7d511198a36e867b
hollaze <deperiers.a@gmail.com>
2021-08-23 19:03:00 +0200

    search api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""search api"""
+import json
+from requests import post
+from sys import argv
+
+if __name__ == "__main__":
+
+    if len(argv) <= 1:
+        value = ''
+    else:
+        value = argv[1]
+
+    datas = {'q': value}
+    json_response = post(" http://ac5942ef3eb7.c0cd6e92.hbtn-cod.io:5000/search_user", data=datas).json()
+
+    try:
+        if json_response:
+            print("[{}] {}".format(json_response["id"], json_response["name"]))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
