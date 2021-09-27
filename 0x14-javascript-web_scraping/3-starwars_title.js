a8767748dd1d34857c53bcf9bbaa77366f040a30
hollaze <deperiers.a@gmail.com>
2021-09-27 12:09:39 +0200

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,6 @@ const episodeNumber = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + episodeNumber;
 
 request(url, function (error, response, body) {
-    if (error) throw error;
-    console.log(JSON.parse(body).title);
+  if (error) throw error;
+  console.log(JSON.parse(body).title);
 });
