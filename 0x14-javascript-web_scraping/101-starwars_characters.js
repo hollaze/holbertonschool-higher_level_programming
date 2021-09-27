c51dbe748a2e506e6ad5d8bfc15500364d56ace5
hollaze <deperiers.a@gmail.com>
2021-09-27 15:05:01 +0200

    in right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,13 +3,16 @@ const movieNumber = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movieNumber;
 const request = require('request');
 
-request(url, function (error, response, body) {
+request(url, async (error, response, body) => {
   if (error) throw error;
   const characters = JSON.parse(body).characters;
   for (let i = 0; i < characters.length; i++) {
-    request(characters[i], function (error, response, body) {
-      if (error) throw error;
-      console.log(JSON.parse(body).name);
+    const name = await new Promise((resolve, reject) => {
+      request(characters[i], function (error, response, body) {
+        if (error) return reject(error);
+        resolve(JSON.parse(body).name);
+      });
     });
+    console.log(name);
   }
 });
