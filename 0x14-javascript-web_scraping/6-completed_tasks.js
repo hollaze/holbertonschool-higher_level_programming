14327507f20f72ac2c9d583fc45fbdc5e8dc5e96
hollaze <deperiers.a@gmail.com>
2021-09-27 21:26:12 +0200

    number of completed tasks by userId

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2]; // https://jsonplaceholder.typicode.com/todos
+const completedTasks = {};
 
 request(url, function (error, response, body) {
   if (error)  console.log(error);
   const parsedBody = JSON.parse(body);
 
-  const completedTasks = {};
-
   for (let i = 1; i < parsedBody.length; i++) {
-    if (parsedBody[i].completed) {
-      if (parsedBody[i].userId !== undefined) {
+    if (parsedBody[i].completed === true) {
+      if (parsedBody[i].userId in completedTasks) {
         completedTasks[parsedBody[i].userId] += 1;
       } else {
         completedTasks[parsedBody[i].userId] = 1;
