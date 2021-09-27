6ae1c681e31ce53a5f4d0445d700139d8eb95945
hollaze <deperiers.a@gmail.com>
2021-09-27 21:12:42 +0200

    number of completed tasks by userId

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2]; // https://jsonplaceholder.typicode.com/todos
+const completedTasks = {};
+
+request(url, function (error, response, body) {
+  if (error) throw error;
+  const parsedBody = JSON.parse(body);
+
+  for (let i = 1; i < parsedBody.length; i++) {
+    if (parsedBody[i].completed === true) {
+      if (parsedBody[i].userId in completedTasks) {
+        completedTasks[parsedBody[i].userId] += 1;
+      } else {
+        completedTasks[parsedBody[i].userId] = 1;
+      }
+    }
+  }
+  console.log(completedTasks);
+});
