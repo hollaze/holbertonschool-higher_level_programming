9089d711e601c901cbb71685034c7bfa3cf2aa51
hollaze <deperiers.a@gmail.com>
2021-09-28 10:00:06 +0200

    now using films url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-const character = 'https://swapi-api.hbtn.io/api/people/18/'; // Wedge Antilles
 const url = process.argv[2];
-const episodeId = [];
 let count = 0;
 
 request(url, function (error, response, body) {
   if (error) return (console.log(error));
   const results = JSON.parse(body).results;
-  for (let i = 0; i < results.length; i++) {
-    for (let j = 0; j < results[i].characters.length; j++) {
-      if (results[i].characters[j] === character &&
-        !(results[i].episode_id in episodeId)) {
+  results.forEach(episodeId => {
+    episodeId.characters.forEach(character => {
+      if (character.indexOf('people/18') >= 0) {
         count += 1;
       }
-    }
-  }
-
+    });
+  });
   console.log(count);
 });
