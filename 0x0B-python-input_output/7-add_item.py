9d7ac770590263dc23d6c58f3c44042c981f63a0
hollaze <deperiers.a@gmail.com>
2021-10-02 20:55:32 +0200

    check

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ add as many arguments into a list in json file """
+
 import sys
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
