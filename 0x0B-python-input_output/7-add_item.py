9b099bc47c74cd048a5fffe6c78dfc39c311d91f
hollaze <deperiers.a@gmail.com>
2021-10-02 20:53:04 +0200

    check

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" add as many arguments into a list in json file """
+import sys
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+
+file_name = "add_item.json"
+list = []
+
+list = load_from_json_file(file_name)
+
+for argument in sys.argv[1:]:
+    list.append(argument)
+
+save_to_json_file(list, file_name)
