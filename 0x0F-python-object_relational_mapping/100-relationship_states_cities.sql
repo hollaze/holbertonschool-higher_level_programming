89a5ebaae690e302800e7c8500ed385f932a64c0
hollaze <deperiers.a@gmail.com>
2021-10-09 01:00:33 +0200

    sql file

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.sql
@@ -0,0 +1,7 @@
+guillaume@ubuntu:~/0x0F$ cat 100-relationship_states_cities.sql
+-- Create the database hbtn_0e_100_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0e_100_usa;
+USE hbtn_0e_100_usa;
+
+SELECT * FROM states;
+SELECT * FROM cities;
