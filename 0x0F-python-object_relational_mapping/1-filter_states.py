58f5c06f6e3ca68696f526342c82a48b72dd5d96
hollaze <deperiers.a@gmail.com>
2021-10-08 16:00:38 +0200

    fix for uppercase letter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id ASC")
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%'")
 
     for row in cursor.fetchall():
         print(row)
